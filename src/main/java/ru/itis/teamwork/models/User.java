09a2f5a3558ead0b3719092ab72e5faadb3e5338
matveymaletskov <maletskovitis@mail.ru>
2019-02-21 10:11:58 +0300

    c3

--- src/main/java/ru/itis/teamwork/models/User.java
+++ src/main/java/ru/itis/teamwork/models/User.java
@@ -4,16 +4,14 @@ public class User {
     private Integer id;
     private String firstName;
     private String lastName;
-    private Integer age;
 
     public User() {
 
     }
 
-    public User(String firstName, String lastName, Integer age) {
+    public User(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.age = age;
     }
 
     public User(Integer id, String firstName, String lastName) {
@@ -44,13 +42,5 @@ public class User {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
     }
 }
