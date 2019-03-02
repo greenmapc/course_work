78ab22a5090bae63dcd2fde122e29db4dad249d2
matveymaletskov <maletskovitis@mail.ru>
2019-03-02 21:39:16 +0300

    add some github

--- src/main/java/ru/itis/teamwork/services/GitHubApi.java
+++ src/main/java/ru/itis/teamwork/services/GitHubApi.java
@@ -12,64 +12,71 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
+import javax.annotation.Resource;
 
+@PropertySource("classpath:/git.properties")
 public class GitHubApi {
+    @Resource
+    private Environment env;
 
-    private final String CLIENT_ID = "b8e1772648afd427f8cc";
-    private final String CLIENT_SECRET = "df0f334bc2c424539e911af3b6be9f8a11f8ecc3";
-    private final String GITHUB_API_AUTH = "https://github.com/login/oauth/";
-    private final String REDIRECT = "http://localhost:8080/gitCode";
-    private final String GITHUB = "https://api.github.com/user/";
+    private final String CLIENT_ID;
+    private final String CLIENT_SECRET;
+    private final String GITHUB_API_AUTH;
+    private final String REDIRECT;
+    private final String GITHUB;
 
     private HttpClient httpClient;
 
-    public GitHubApi(){
+    public GitHubApi() {
         this.httpClient = HttpClients.createDefault();
+        this.CLIENT_ID = env.getRequiredProperty("CLIENT_ID");
+        this.CLIENT_SECRET = env.getRequiredProperty("CLIENT_SECRET");
+        this.GITHUB_API_AUTH = env.getRequiredProperty("GITHUB_API_AUTH");
+        this.REDIRECT = env.getRequiredProperty("REDIRECT");
+        this.GITHUB = env.getRequiredProperty("GITHUB");
     }
 
     @SneakyThrows
-    public String getAuthLink(){
-
+    public String getAuthLink() {
         URIBuilder uriBuilder = new URIBuilder(GITHUB_API_AUTH.concat(Operation.AUTHORIZE.operation));
         uriBuilder.addParameter(Param.client_id.param, CLIENT_ID)
                 .addParameter(Param.scope.param, "repo, gist, admin, user, delete_repo, write:discussion, admin:gpg_key")
-                .addParameter(Param.redirect_uri.param,REDIRECT );
+                .addParameter(Param.redirect_uri.param, REDIRECT);
 
         return uriBuilder.build().toString();
     }
 
     @SneakyThrows
-    public String getAccessToken(String code){
-
+    public String getAccessToken(String code) {
         URIBuilder uriBuilder = new URIBuilder(GITHUB_API_AUTH.concat(Operation.ACCESS_TOKEN.operation));
 
         uriBuilder.setParameter(Param.client_id.param, CLIENT_ID)
                 .setParameter(Param.client_secret.param, CLIENT_SECRET)
-                .setParameter(Param.redirect_uri.param, REDIRECT )
+                .setParameter(Param.redirect_uri.param, REDIRECT)
                 .setParameter(Param.code.param, code);
 
         HttpPost httpPost = new HttpPost(uriBuilder.build());
         httpPost.setHeader(HttpHeaders.ACCEPT, "application/json");
-        JSONObject obj = this.getJsonResp( httpClient.execute(httpPost)).getJSONObject(0) ;
-
+        JSONObject obj = this.getJsonResp(httpClient.execute(httpPost)).getJSONObject(0);
 
         return obj.getString("access_token");
-
     }
 
     @SneakyThrows
-    private JSONArray getJsonResp(HttpResponse response){
+    private JSONArray getJsonResp(HttpResponse response) {
         HttpEntity httpEntity = response.getEntity();
         String jsonRes = EntityUtils.toString(httpEntity);
-        if (jsonRes.charAt(0) != '['){
+        if (jsonRes.charAt(0) != '[') {
             jsonRes = ("[").concat(jsonRes).concat("]");
         }
         return new JSONArray(jsonRes);
     }
 
     @SneakyThrows
-    public String getUserEmail(String token){
+    public String getUserEmail(String token) {
         URIBuilder uriBuilder = new URIBuilder(GITHUB.concat("emails"));
         HttpGet httpGet = new HttpGet(uriBuilder.build());
         httpGet.addHeader(HttpHeaders.AUTHORIZATION, "token ".concat(token));
@@ -77,19 +84,28 @@ public class GitHubApi {
         return obj.getString("email");
     }
 
-    private enum Operation{
+    @SneakyThrows
+    public String getUsername(String token) {
+        URIBuilder uriBuilder = new URIBuilder(GITHUB);
+        HttpGet httpGet = new HttpGet(uriBuilder.build());
+        httpGet.addHeader(HttpHeaders.AUTHORIZATION, "token ".concat(token));
+        JSONObject obj = this.getJsonResp(httpClient.execute(httpGet)).getJSONObject(0);
+        return obj.getString("login");
+    }
+
+    private enum Operation {
         AUTHORIZE("authorize"),
         ACCESS_TOKEN("access_token");
 
         private String operation;
 
-        Operation(String operation){
+        Operation(String operation) {
             this.operation = operation;
         }
 
     }
 
-    private enum Param{
+    private enum Param {
         client_id("client_id"),
         client_secret("client_secret"),
         redirect_uri("redirect_uri"),
@@ -98,7 +114,7 @@ public class GitHubApi {
 
         private String param;
 
-        Param(String param){
+        Param(String param) {
             this.param = param;
         }
     }
