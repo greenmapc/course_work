5da5a1f67e52832cdcb8c8ab49f8044ac7c1c299
Danis Zamaliev <daniszamaliev@gmail.com>
2019-03-06 13:10:51 +0300

    remove unworking code

--- src/main/java/ru/itis/teamwork/config/WebSecurityConfig.java
+++ src/main/java/ru/itis/teamwork/config/WebSecurityConfig.java
@@ -3,33 +3,15 @@ package ru.itis.teamwork.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.endpoint.NimbusAuthorizationCodeTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
-import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
-import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import ru.itis.teamwork.services.UserService;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @EnableWebSecurity
 @ComponentScan(value = {"ru.itis.teamwork.services", "ru.itis.teamwork.controllers"})
@@ -42,24 +24,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private ClientRegistrationRepository clientRegistrationRepository;
-
-    @Autowired
-    private OAuth2AuthorizedClientService authorizedClientService;
-
-    @Autowired
-    private AuthorizationRequestRepository<OAuth2AuthorizationRequest> authorizationRequestRepository;
-
-    @Autowired
-    private OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        /*http
+        http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/registration", "/gitCode", "/gitAuth")
+                .antMatchers("/", "/registration")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
@@ -69,90 +39,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
-                .permitAll();*/
-        http.authorizeRequests()
-                .antMatchers("/oauth__login")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .oauth2Login()
-                .loginPage("/oauth__login")
-                .defaultSuccessUrl("/loginSuccess")
-                .failureUrl("/loginFailure")
-                .clientRegistrationRepository(clientRegistrationRepository())
-                .authorizedClientService(authorizedClientService())
-                .authorizationEndpoint()
-                .baseUri("/oauth2/authorize-client")
-                .authorizationRequestRepository(authorizationRequestRepository())
-                .and()
-                .tokenEndpoint()
-                .accessTokenResponseClient(accessTokenResponseClient())
-                .and()
-                .redirectionEndpoint()
-                .baseUri("/oauth2/redirect");
-    }
-
-    @Bean
-    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest>
-    accessTokenResponseClient() {
-
-        return new NimbusAuthorizationCodeTokenResponseClient();
-    }
-
-    @Bean
-    public AuthorizationRequestRepository<OAuth2AuthorizationRequest>
-    authorizationRequestRepository() {
-
-        return new HttpSessionOAuth2AuthorizationRequestRepository();
-    }
-
-    private static List<String> clients = Arrays.asList("github");
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        List<ClientRegistration> registrations = clients.stream()
-                .map(c -> getRegistration(c))
-                .filter(registration -> registration != null)
-                .collect(Collectors.toList());
-
-        return new InMemoryClientRegistrationRepository(registrations);
-    }
-
-    @Bean
-    @Lazy
-    public OAuth2AuthorizedClientService authorizedClientService() {
-        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository());
-    }
-
-    private static String CLIENT__PROPERTY__KEY
-            = "spring.security.oauth2.client.registration.";
-
-    @Autowired
-    private Environment env;
-
-    private ClientRegistration getRegistration(String client) {
-        String clientId = env.getProperty(
-                CLIENT__PROPERTY__KEY + client + ".client-id");
-
-        if (clientId == null) {
-            return null;
-        }
-
-        String clientSecret = env.getProperty(
-                CLIENT__PROPERTY__KEY + client + ".client-secret");
-
-        if (client.equals("github")) {
-            return CommonOAuth2Provider.GITHUB.getBuilder(client)
-                    .clientId(clientId).clientSecret(clientSecret).build();
-        }
-        return null;
+                .permitAll();
     }
 
 
 
 
-/*
+
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
@@ -163,5 +56,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService)
                 .passwordEncoder(passwordEncoder);
-    }*/
+    }
 }
